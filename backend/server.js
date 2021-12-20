9eb0e772b0b000903555c154740fdab75b65f390
Jason <snujugroix@gmail.com>
2021-12-20 16:52:21 -0500

    finished dotenv intro

--- backend/server.js
+++ backend/server.js
@@ -19,4 +19,9 @@ app.get("/api/products/:id", (req, res) => {
 	res.json(product);
 });
 
-app.listen(5000, console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+	PORT,
+	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
